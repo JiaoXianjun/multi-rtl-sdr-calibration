0a4940a119d39a54a44defdb41d4c49d016ef0e3
ryan.jiao@nokia.com <ryan.jiao@nokia.com>
2014-01-08 15:01:18 +0800

    now freaze testing rtl_tcp and begin to continue gsm based multi dongles calibration

--- scan_band_power_spectrum_tcp.m
+++ scan_band_power_spectrum_tcp.m
@@ -7,8 +7,13 @@
 % ATTENTION! every time before you run this script, please terminate two rtl_tcp and re-launch them again.
 % Then set all parameters like:
 
-% start_freq = 930e6;
-% end_freq = 932e6;
+num_dongle = 2; % change it according to the number of dongles you have
+
+% start_freq = 913e6;
+% end_freq = 917e6;
+
+% start_freq = 1875e6;
+% end_freq = 1905e6;
 
 start_freq = 935e6; % P GSM
 % start_freq = 925e6; % E GSM
@@ -18,13 +23,13 @@ end_freq = 960e6;
 
 freq_step = 100e3;
 
-RBW = 50e3;
+RBW = 50e3; % resolution bandwidth each time we inspect
 
 gain = 49; % 49.6dB is the maximum value for 820T tuner. You should find a appropriate value for your case
 
-observe_time = 500e-3; % observation time at each frequency point
+observe_time = 1; % observation time at each frequency point
 
-sample_rate = 1e6;
+sample_rate = 2.048e6;
 
 coef = fir1(31, RBW/sample_rate);
 
@@ -33,85 +38,109 @@ packet_len = 8192;
 num_samples = observe_time*sample_rate;
 num_recv = ceil(2*num_samples/packet_len);
 
-if ~isempty(who('tcp_obj0'))
-    fclose(tcp_obj0);
-    delete(tcp_obj0);
-    clear tcp_obj0;
+if ~isempty(who('tcp_obj'))
+    for i=1:length(tcp_obj)
+        fclose(tcp_obj{i});
+        delete(tcp_obj{i});
+    end
+    clear tcp_obj;
 end
 
-if ~isempty(who('tcp_obj1'))
-    fclose(tcp_obj1);
-    delete(tcp_obj1);
-    clear tcp_obj1;
+tcp_obj = cell(1, num_dongle);
+for i=1:num_dongle
+    tcp_obj{i} = tcpip('127.0.0.1', 1233+i); % for dongle i
 end
-
-tcp_obj0 = tcpip('127.0.0.1', 1234); % for dongle 0
-tcp_obj1 = tcpip('127.0.0.1', 1235); % for dongle 1
 
 fread_len = packet_len;
-set(tcp_obj0, 'InputBufferSize', 4*num_recv*fread_len);
-set(tcp_obj0, 'Timeout', 40);
-set(tcp_obj1, 'InputBufferSize', 4*num_recv*fread_len);
-set(tcp_obj1, 'Timeout', 40);
+for i=1:num_dongle
+    set(tcp_obj{i}, 'InputBufferSize', 4*num_recv*fread_len);
+    set(tcp_obj{i}, 'Timeout', 40);
+    fopen(tcp_obj{i});
+end
 
-fopen(tcp_obj0);
-fopen(tcp_obj1);
 clf;
 close all;
+
 num_frame = num_recv;
 
-power_spectrum0 = inf.*ones(1, length(start_freq:freq_step:end_freq));
-power_spectrum1 = inf.*ones(1, length(start_freq:freq_step:end_freq));
+power_spectrum = inf.*ones(num_dongle, length(start_freq:freq_step:end_freq));
 
-set_gain_tcp(tcp_obj0, gain*10); %be careful, in rtl_sdr the 10x is done inside C program, but in rtl_tcp the 10x has to be done here.
-set_gain_tcp(tcp_obj1, gain*10);
+% set gain
+for i=1:num_dongle
+    set_gain_tcp(tcp_obj{i}, gain*10); %be careful, in rtl_sdr the 10x is done inside C program, but in rtl_tcp the 10x has to be done here.
+end
 
-set_rate_tcp(tcp_obj0, sample_rate);
-set_rate_tcp(tcp_obj1, sample_rate);
+% set sampling rate
+for i=1:num_dongle
+    set_rate_tcp(tcp_obj{i}, sample_rate);
+end
 
-set_freq_tcp(tcp_obj0, start_freq);
-set_freq_tcp(tcp_obj1, start_freq);
-fread(tcp_obj0, 4*num_recv*fread_len, 'uint8');
-fread(tcp_obj1, 4*num_recv*fread_len, 'uint8');
+% set frequency
+for i=1:num_dongle
+    set_freq_tcp(tcp_obj{i}, start_freq);
+end
+
+% read and discard to flush
+for i=1:num_dongle
+    fread(tcp_obj{i}, 4*num_recv*fread_len, 'uint8');
+end
+
+% read and discard to flush
+for i=1:num_dongle
+    fread(tcp_obj{i}, 4*num_recv*fread_len, 'uint8');
+end
+
 pause(1);
 
 idx = 1;
+s = zeros(num_frame*fread_len, num_dongle);
+tmp = zeros(fread_len, num_dongle);
+real_count = zeros(1, num_dongle);
 for freq = start_freq:freq_step:end_freq
-    set_freq_tcp(tcp_obj0, freq);
-    set_freq_tcp(tcp_obj1, freq);
-
-    a0 = inf.*ones(fread_len, num_frame);
-    a1 = inf.*ones(fread_len, num_frame);
+    % set target frequency
+    for i=1:num_dongle
+        set_freq_tcp(tcp_obj{i}, freq);
+    end
     
-    i = 1;
+    % read and discard to flush
+    for i=1:num_dongle
+        fread(tcp_obj{i}, num_recv*fread_len, 'uint8');
+    end
+
+    frame_idx = 1;
     while 1
-        [tmp0, real_count0] = fread(tcp_obj0, fread_len, 'uint8');
-        [tmp1, real_count1] = fread(tcp_obj1, fread_len, 'uint8');
+        for i=1:num_dongle
+            [tmp(:,i), real_count(i)] = fread(tcp_obj{i}, fread_len, 'uint8');
+        end
         
-        if ( real_count0~=fread_len || real_count1~=fread_len )
-            disp(num2str([idx i fread_len, real_count0, real_count1]));
+        if sum(real_count-fread_len) ~= 0
+            disp(num2str([idx frame_idx fread_len, real_count]));
             continue;
         end
         
-        a0(:, i) = tmp0;
-        a1(:, i) = tmp1;
+        for i=1:num_dongle
+            s( ((frame_idx-1)*fread_len + 1) : (frame_idx*fread_len), i) = tmp(:,i);
+        end
         
-        if i == num_frame
+        if frame_idx == num_frame
             break;
         end
         
-        i = i + 1;
+        frame_idx = frame_idx + 1;
     end
-
+    
     % convert raw unsigned IQ samples to normal IQ samples for signal processing purpose
-    s = raw2iq([a0(:), a1(:)]);
+    s = raw2iq(s);
     
     % process samples from two dongles in two columns of varable s
     % add your routine here
     s = filter(coef, 1, s);
 
-    power_spectrum0(idx) = 10*log10(mean(abs(s(:,1)).^2));
-    power_spectrum1(idx) = 10*log10(mean(abs(s(:,2)).^2));
+    % get averaged power and convert it to dB
+    for i=1:num_dongle
+        power_spectrum(i, idx) = 10*log10(mean(abs(s(:,i)).^2));
+    end
+
     idx = idx + 1;
     
     if mod(idx, 50) == 0
@@ -124,14 +153,20 @@ for freq = start_freq:freq_step:end_freq
 %     drawnow;
 end
 
-plot((start_freq:freq_step:end_freq).*1e-6, power_spectrum0, 'b.-'); hold on;
-plot((start_freq:freq_step:end_freq).*1e-6, power_spectrum1, 'r.-'); hold on; grid on;
-legend('dongle 0', 'dongle 1');
+% plot power spectrum
+format_string = {'b.-', 'r.-', 'k.-', 'm.-'};
+for i=1:num_dongle
+    plot((start_freq:freq_step:end_freq).*1e-6, power_spectrum(i,:), format_string{i}); hold on;
+end
 
-fclose(tcp_obj0);
-delete(tcp_obj0);
-clear tcp_obj0;
+legend_string = cell(1, num_dongle);
+for i=1:num_dongle
+    legend_string{i} = ['dongle ' num2str(i)];
+end
+legend(legend_string);
 
-fclose(tcp_obj1);
-delete(tcp_obj1);
-clear tcp_obj1;
+for i=1:num_dongle
+    fclose(tcp_obj{i});
+    delete(tcp_obj{i});
+end
+clear tcp_obj;
