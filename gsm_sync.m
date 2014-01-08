69d29845a3448e8c6d443556882bb958322609c4
Jiao Xianjun(Ryan) <putaoshu@msn.com>
2014-01-08 17:57:22 +0800

    gsm_sync now works quite well with rtl_tcp now

--- gsm_sync.m
+++ gsm_sync.m
@@ -1,19 +1,14 @@
 % Jiao Xianjun (putaoshu@msn.com; putaoshu@gmail.com)
-% try to have two dongles synchronized to the same GSM downlink FCCH SCH
-% run command line first: ./rtl-sdr-relay -b 8192 -l 8192
+% try to have multiple dongles synchronized to the same GSM downlink FCCH SCH
+% run command line first (depends on how many dongles you have):
+% rtl_tcp -p 1234 -d 0
+% rtl_tcp -p 1235 -d 1
+% rtl_tcp -p 1236 -d 2
+% ...
 
-% freq = 957.4e6;
-freq = 957.6e6;
-% freq = 957.8e6;
+num_dongle = 2;
 
-% freq = 956.2e6;
-% freq = 956.4e6;
-% freq = 956.6e6;
-
-% freq = 942.6e6;
-% freq = 939.2e6;
-% freq = 942.8e6;
-% freq = 958.8e6;
+freq = 957.4e6; % find some GSM like signal by scan_band_power_spectrum_tcp.m!
 
 symbol_rate = (1625/6)*1e3;
 oversampling_ratio = 4;
@@ -24,64 +19,78 @@ packet_len = 8192; % this value must be comformed with paramter -l of rtl-sdr-re
 
 num_frame = ceil( (inspection_time*sampling_rate*2)/packet_len );
 
-if ~isempty(who('udp_obj0'))
-    fclose(udp_obj0);
-    delete(udp_obj0);
-    clear udp_obj0;
+if ~isempty(who('tcp_obj'))
+    for i=1:length(tcp_obj)
+        fclose(tcp_obj{i});
+        delete(tcp_obj{i});
+    end
+    clear tcp_obj;
 end
 
-if ~isempty(who('udp_obj1'))
-    fclose(udp_obj1);
-    delete(udp_obj1);
-    clear udp_obj1;
+tcp_obj = cell(1, num_dongle);
+for i=1:num_dongle
+    tcp_obj{i} = tcpip('127.0.0.1', 1233+i); % for dongle i
 end
-
-udp_obj0 = udp('127.0.0.1', 13485, 'LocalPort', 6666); % for dongle 0
-udp_obj1 = udp('127.0.0.1', 13485, 'LocalPort', 6667); % for dongle 1
 
 fread_len = packet_len;
-set(udp_obj0, 'InputBufferSize', 2*num_frame*fread_len);
-set(udp_obj0, 'Timeout', 40);
-set(udp_obj1, 'InputBufferSize', 2*num_frame*fread_len);
-set(udp_obj1, 'Timeout', 40);
-% time_to_flush_buffer = (2.1*num_frame*fread_len/2)/sample_rate;
+for i=1:num_dongle
+    set(tcp_obj{i}, 'InputBufferSize', 4*num_frame*fread_len);
+    set(tcp_obj{i}, 'Timeout', 40);
+    fopen(tcp_obj{i});
+end
 
-fopen(udp_obj0);
-fopen(udp_obj1);
 clf;
 close all;
 
 % set frequency gain and sampling rate
-fwrite(udp_obj0, int32(round([freq, 0, sampling_rate])), 'int32');
-pause(time_to_flush_buffer);
+% set sampling rate
+for i=1:num_dongle
+    set_rate_tcp(tcp_obj{i}, sampling_rate);
+end
+
+% set frequency
+for i=1:num_dongle
+    set_freq_tcp(tcp_obj{i}, freq);
+end
+
+% % read and discard to flush
+% for i=1:num_dongle
+%     fread(tcp_obj{i}, 4*num_frame*fread_len, 'uint8');
+% end
+% 
+% % read and discard to flush
+% for i=1:num_dongle
+%     fread(tcp_obj{i}, 4*num_frame*fread_len, 'uint8');
+% end
+% 
+% pause(1);
 
 sampling_rate_4x = sampling_rate;
 idx = 1;
+s = zeros(num_frame*fread_len, num_dongle);
+tmp = zeros(fread_len, num_dongle);
+real_count = zeros(1, num_dongle);
 while 1
-%     % set frequency gain and sampling rate
-%     fwrite(udp_obj0, int32(round([freq, 0, sampling_rate])), 'int32');
-%     pause(time_to_flush_buffer);
-    
-    a0 = inf.*ones(fread_len, num_frame);
-    a1 = inf.*ones(fread_len, num_frame);
     good_flag = true;
-    for i=1:num_frame  % get many frame in one signal sequence
-        [tmp0, real_count0] = fread(udp_obj0, fread_len, 'uint8');
-        [tmp1, real_count1] = fread(udp_obj1, fread_len, 'uint8');
+    for frame_idx=1:num_frame  % get many frame in one signal sequence
+        for i=1:num_dongle
+            [tmp(:,i), real_count(i)] = fread(tcp_obj{i}, fread_len, 'uint8');
+        end
         
-        if ( real_count0~=fread_len || real_count1~=fread_len )
+        if sum(real_count-fread_len) ~= 0
             good_flag = false;
-            disp(num2str([idx i fread_len, real_count0, real_count1]));
+            disp(num2str([idx frame_idx fread_len, real_count]));
             break;
         end
         
-        a0(:, i) = tmp0;
-        a1(:, i) = tmp1;
+        for i=1:num_dongle
+            s( ((frame_idx-1)*fread_len + 1) : (frame_idx*fread_len), i) = tmp(:,i);
+        end
     end
 
     if good_flag
         % convert raw unsigned IQ samples to normal IQ samples for signal processing purpose
-        s = raw2iq([a0(:), a1(:)]);
+        s = raw2iq(s);
 
         % process signal
 %         s = chn_filter_8x_4x(s);
@@ -89,11 +98,18 @@ while 1
 
         [FCCH_pos, metric_data] = FCCH_coarse_position(s, sampling_rate_4x);
 
-        subplot(2,2,1); plot(FCCH_pos(:,1));
-        subplot(2,2,2); plot(metric_data(:,1));
-        subplot(2,2,3); plot(FCCH_pos(:,2));
-        subplot(2,2,4); plot(metric_data(:,2));
+        for i=1:num_dongle
+            figure(i);
+            subplot(2,1,1); plot(FCCH_pos(:,i));
+            subplot(2,1,2); plot(metric_data(:,i));
+        end
         drawnow;
     end
     idx = idx + 1;
 end
+
+for i=1:num_dongle
+    fclose(tcp_obj{i});
+    delete(tcp_obj{i});
+end
+clear tcp_obj;
