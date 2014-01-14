fc6d9312f14a2fb5cbbcb183eab87333cd499488
Jiao Xianjun(Ryan) <putaoshu@msn.com>
2014-01-14 14:29:21 +0800

    end development of multi_rtl_sdr_diversity_scanner.m

--- multi_rtl_sdr_diversity_scanner_another_bak.m
+++ multi_rtl_sdr_diversity_scanner_another_bak.m
@@ -1,6 +1,7 @@
 % Jiao Xianjun (putaoshu@msn.com; putaoshu@gmail.com)
 % Frequency band scanning and incoherent combination via multiple rtl-sdr dongles.
 % A script of project: https://github.com/JiaoXianjun/multi-rtl-sdr-calibration
+% Temporary save for another fast scanning method. Not complete yet.
 
 % Assume that you have installed rtl-sdr
 % (http://sdr.osmocom.org/trac/wiki/rtl-sdr) and have those native utilities run correctly already.
@@ -21,22 +22,22 @@
 num_dongle = 1;
 
 % Beginning of the band you are interested in
-% start_freq = 910e6; % for test
-start_freq = 935e6; % Beginning of Primary GSM-900 Band downlink
+start_freq = 910e6; % for test
 % start_freq = (1575.42-15)*1e6; % GPS L1
 % start_freq = (1207.14-30)*1e6; % COMPASS B2I
 % start_freq = (1561.098-30)*1e6; % COMPASS B1I
+% start_freq = 935e6; % Beginning of Primary GSM-900 Band downlink
 % start_freq = 1.14e9; % Beginning of GNSS(GPS/GLONASS/COMPASS/Galileo) Band
 
 % End of the band you are interested in
-% end_freq = 920e6; % for test
-end_freq = 960e6; % End of Primary GSM-900 Band downlink
+end_freq = 920e6; % for test
 % end_freq = (1575.42+30)*1e6; % GPS L1
 % end_freq = (1207.14+30)*1e6; % COMPASS B2I
 % end_freq = (1561.098+30)*1e6; % COMPASS B1I
+% end_freq = 960e6; % End of Primary GSM-900 Band downlink
 % end_freq = 1.63e9; % Beginning of GNSS(GPS/GLONASS/COMPASS/Galileo) Band
 
-freq_step = 0.05e6; % less step, higher resolution, narrower FIR bandwidth, slower speed
+freq_step = 0.1e6; % less step, higher resolution, narrower FIR bandwidth, slower speed
 
 observe_time = 0.2; % observation time at each frequency point. ensure it can capture your signal!
 
@@ -44,9 +45,9 @@ RBW = freq_step; % Resolution Bandwidth each time we inspect
 
 gain = 0; % If this is larger than 0, the fixed gain will be set to dongles
 
-% use high sampling rate and FIR to improve estimation accuracy
+% use high sampling rate and narrow FIR to improve estimation accuracy
 sample_rate = 2.048e6; % sampling rate of dongles
-
+% construct a FIR to be used to extract target bandwidth
 coef_order = (2^(ceil(log2(sample_rate/RBW))))-1;
 coef_order = min(coef_order, 127);
 coef_order = max(coef_order, 31);
@@ -55,14 +56,58 @@ coef = fir1(coef_order, RBW/sample_rate);
 
 num_samples = observe_time*sample_rate;
 
+% pre calculate something to be used for extracting multiple freqeuncy channels with in one sampling stream.
 clf;
 close all;
 
 real_count = zeros(1, num_dongle);
 freq = start_freq:freq_step:end_freq;
+real_freq_step = sample_rate/4;
+real_freq = start_freq:real_freq_step:end_freq;
+if real_freq(end)+(real_freq_step/2) < freq(end)
+    real_freq = [real_freq, real_freq(end)+real_freq_step];
+end
 
-s_all = uint8( zeros(2*num_samples, length(freq), num_dongle) );
-decimate_ratio = floor(sample_rate/(2*RBW));
+freq_info(1:length(real_freq)) = struct('center_freq', inf, 'num_sub_freq', inf, 'sub_freq_set', inf.*ones(1, ceil(real_freq_step/freq_step)+1), 'relative_sub_freq_set', inf.*ones(1, ceil(real_freq_step/freq_step)+1));
+
+for i=1:length(real_freq)
+    freq_info(i).center_freq = real_freq(i);
+    freq_start = real_freq(i) - (real_freq_step/2);
+    freq_end = real_freq(i) + (real_freq_step/2);
+    freq_set = find(freq>freq_start & freq<=freq_end);
+    freq_info(i).num_sub_freq = length(freq_set);
+    freq_info(i).sub_freq_set(1:length(freq_set)) = freq(freq_set);
+    freq_info(i).relative_sub_freq_set(1:length(freq_set)) = freq(freq_set) - real_freq(i);
+end
+% % % -------------------------------------verify-----------------------------------
+% figure;
+% plot(freq, 'b.'); hold on; grid on;
+% tmp = [];
+% for i=1:length(real_freq)
+%     tmp = [tmp freq_info(i).center_freq];
+% end
+% plot(tmp, 'rs');
+% tmp = [];
+% for i=1:length(real_freq)
+%     tmp = [tmp freq_info(i).sub_freq_set(1:freq_info(i).num_sub_freq)];
+% end
+% plot(tmp, 'kv');
+% 
+% figure;
+% plot(tmp-freq); grid on; hold on;
+% tmp = [];
+% for i=1:length(real_freq)
+%     tmp = [tmp freq_info(i).relative_sub_freq_set(1:freq_info(i).num_sub_freq) + freq_info(i).center_freq];
+% end
+% plot(tmp-freq, 'r.');
+% 
+% figure;
+% tmp = [];
+% for i=1:length(real_freq)
+%     tmp = [tmp freq_info(i).num_sub_freq];
+% end
+% plot(tmp, 'b.'); hold on; grid on;
+% % % ----------------------------end of verify-----------------------------------
 
 % check if previous tce objects existed. if so clear them
 if ~isempty(who('tcp_obj'))
@@ -106,14 +151,17 @@ for i=1:num_dongle
     fread(tcp_obj{i}, 8*2*num_samples, 'uint8');
 end
 
-% capture samples of all frequencies firstly!
-for freq_idx = 1:length(freq)
-    current_freq = freq(freq_idx);
+% try to read all frequencies firstly!
+s = zeros(2*num_samples, num_dongle);
+r_all = zeros(num_samples, num_dongle, length(real_freq));
+for real_freq_idx = 1:length(real_freq)
     
-    while 1 % read data at current frequency until success
+    current_freq = real_freq(real_freq_idx);
+    
+    while 1 % read data
         for i=1:num_dongle
             set_freq_tcp(tcp_obj{i}, current_freq); % set current frequency
-            [s_all(:,freq_idx,i), real_count(i)] = fread(tcp_obj{i}, 2*num_samples, 'uint8');
+            [s(:,i), real_count(i)] = fread(tcp_obj{i}, 2*num_samples, 'uint8');
         end
 
         if sum(real_count-(2*num_samples)) ~= 0
@@ -122,6 +170,9 @@ for freq_idx = 1:length(freq)
             break;
         end
     end
+    
+    % convert raw unsigned IQ samples to normal IQ samples for signal processing purpose
+    r_all(:,:,real_freq_idx) = raw2iq(s);
 end
 
 for i=1:num_dongle
@@ -131,15 +182,31 @@ end
 clear tcp_obj;
 
 disp('Scanning done!');
-disp(' ');
-disp('Begin process ...');
 
+idx = 1;
 power_spectrum = zeros(num_dongle, length(freq));
-for i=1:num_dongle
-    r = raw2iq( double( s_all(:,:,i) ) ); % remove DC. complex number constructed.
-    r_flt = filter(coef, 1, r);% filter target band out
-    r_flt = r_flt(1:decimate_ratio:end, :);% decimation
-    power_spectrum(i, :) = mean(abs(r_flt).^2, 1);% get averaged power
+for real_freq_idx = 1:length(real_freq)
+    % process samples from multiple dongles in multiple columns of varable s
+    % add your routine here
+    r = r_all(:,:,real_freq_idx);
+    for j = 1 : freq_info(real_freq_idx).num_sub_freq % go through sub frequencies around current freqeuncy
+        % shifting to target sub frequency
+        freq = freq_info(real_freq_idx).relative_sub_freq_set(j);
+        phase_rotate = freq*2*pi/sample_rate;
+        tmp = r.*kron(ones(1, num_dongle), exp(1i.*(1:num_samples).*phase_rotate)');
+        
+        % filter target band out
+        r_flt = filter(coef, 1, tmp);
+
+        % get averaged power
+        for i=1:num_dongle
+            power_spectrum(i, idx) = mean(abs(r_flt(:,i)).^2);
+        end
+        
+        disp(num2str([freq_info(real_freq_idx).sub_freq_set(j) power_spectrum(:, idx).']));
+
+        idx = idx + 1;    
+    end
 end
 
 % plot power spectrum (converted to dB)
